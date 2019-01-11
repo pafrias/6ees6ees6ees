5db49f5fde865cab5a3783c0c25387bc835afc6e
Pablo A Frias <pafrias90@gmail.com>
2019-01-11 10:11:42 -0800

    All Bees updated to ES6 style

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends Bee {
+  constructor() {
+    super();
+    //in all normal circumstances, could be:
+    //  this.age = this.age + 5;
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(value) {
+    this.treasureChest.push(value);
+  }
 };
--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,11 @@
 class Grub {
+  constructor() {
+    this.age = 0;
+    this.color = pink;
+    this.food = jely;
+  }
+  
+  eat() {
+    return `Om Nom ${this.food}!!!`;
+  }
 };
